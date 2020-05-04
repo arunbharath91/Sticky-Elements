6999c61a0413015c8517f8b505916a496f6041a9
Arun Bharath <arunbharath91@gmail.com>
2020-05-04 23:57:53 +0530

    update

--- src/app/sticky.ts
+++ src/app/sticky.ts
@@ -1,16 +1,18 @@
-
+const defaultStyles:string[] = ['position:fixed','z-index:1000','left:0','right:0','top:0','opacity:1','width:100%']
 export class Sticky {
   private element!: HTMLElement;
   private toggleAttr!: string;
-  private stickyStyles: string[] = ['position:fixed','z-index:1000','left:0','right:0','top:0','opacity:1','width:100%'];
+  private stickyStyles!: string[];
+  private computedStyles!: string;
   constructor() {
 
   }
 
-  add(selectorName: string = '[data-fixed]', styles?: string[]) {
+  add(selectorName: string = '[data-fixed]', styles: string[] = []) {
     this.element = document.querySelector(selectorName) as HTMLElement;
     this.toggleAttr = this.element.getAttribute('data-toggle') as string;
-    (styles) ? this.stickyStyles = styles: null;
+    this.stickyStyles = ([...defaultStyles, ...styles]);
+    this.computedStyles = this.stickyStyles.toString().replace(/,/g,';');
     // and then make each element do fixed on scroll
     window.addEventListener("scroll", () =>{
     this.bootSticky();
@@ -21,19 +23,16 @@ export class Sticky {
   }
 
 
-  bootSticky(target: HTMLElement = this.element, toggleAttr: any = this.toggleAttr) {
-
-  if(window.pageYOffset > Number(target.getAttribute("data-fixed")) && !(toggleAttr)){
-  target.setAttribute("style", this.stickyStyles.toString().replace(/,/g,';'));
+  bootSticky() {
+  const triggerSticky:boolean = window.pageYOffset > Number(this.element.getAttribute("data-fixed"));
+  if(this.toggleAttr) {
+    (triggerSticky) ?
+    this.element.classList.add(this.toggleAttr) :
+    this.element.classList.remove(this.toggleAttr);
   } else {
-  target.removeAttribute("style");
+    (triggerSticky) ?
+    this.element.setAttribute("style", this.computedStyles) :
+    this.element.removeAttribute("style");
   }
-
-  if(window.pageYOffset > Number(target.getAttribute("data-fixed")) && toggleAttr){
-  target.classList.add(toggleAttr);
-  } else{
-  target.classList.remove(toggleAttr);
-  }
-
   }
 }
